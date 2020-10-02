ff3122c8d6179a33cae1075c9d82bf6200a16160
Jack Grimes <flangojack@hotmail.com>
2020-10-02 17:53:54 +0100

    Adding a helper class which can be re-used to perform file dialog operations.

---
+++ src/main/java/examples/mvp/utils/FileChooserHelper.java
@@ -0,0 +1,45 @@
+package examples.mvp.utils;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class FileChooserHelper {
+
+
+    // Variables
+
+
+    // Constructors
+
+
+    // Getters and Setters
+
+
+    // Initialisation methods
+
+
+    // Other methods
+    public static File createFile(Stage parentStage) {
+        String title = "Create a file";
+        FileChooser fileChooser = createFileChooser(title);
+        return fileChooser.showSaveDialog(parentStage);
+    }
+
+    public static File openFile(Stage parentStage) {
+        String title = "Select a file to open";
+        FileChooser fileChooser = createFileChooser(title);
+        return fileChooser.showOpenDialog(parentStage);
+    }
+
+    private static FileChooser createFileChooser(String titleText) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(titleText);
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("db files", ".db")
+        );
+        return fileChooser;
+    }
+
+}
