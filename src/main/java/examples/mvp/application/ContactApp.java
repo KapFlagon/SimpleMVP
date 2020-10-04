eb21b06c958987c25096d34b1901da040aa7b66d
Jack Grimes <flangojack@hotmail.com>
2020-10-04 12:26:41 +0100

    Removed redundant code from driver class.

--- src/main/java/examples/mvp/application/ContactApp.java
+++ src/main/java/examples/mvp/application/ContactApp.java
@@ -15,32 +15,10 @@ public class ContactApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         SceneManager sceneManager = new SceneManager(primaryStage);
-
-        BorderPane root = new BorderPane();
-        //FXMLLoader listLoader = new FXMLLoader(getClass().getResource("/examples/mvp/list/list.fxml"));
-        //root.setCenter(listLoader.load());
-        //ListController listController = listLoader.getController();
-//
-        //FXMLLoader editorLoader = new FXMLLoader(getClass().getResource("/examples/mvp/editor/editor.fxml"));
-        //root.setRight(editorLoader.load());
-        //EditorController editorController = editorLoader.getController();
-//
-        //FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("/examples/mvp/menu/menu.fxml"));
-        //root.setTop(menuLoader.load());
-        //MenuController menuController = menuLoader.getController();
-
-        //DataModel model = new DataModel();
-        //listController.initModel(model);
-        //editorController.initModel(model);
-        //menuController.initModel(model);
-
-        Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(sceneManager.getCurrentScene());
         primaryStage.show();
     }
-    
-    // annoying Eclipse launch workaround:
+
     public static void main(String[] args) { launch(args); }
 }
