088160a2c239cf2982349a44eb78615ea175ea52
Jack Grimes <flangojack@hotmail.com>
2020-10-04 12:39:25 +0100

    Added some placeholder code to create buttons from passed path values.

--- src/main/java/examples/mvp/controllers/start_screen/RecentItemsController.java
+++ src/main/java/examples/mvp/controllers/start_screen/RecentItemsController.java
@@ -1,9 +1,20 @@
 package examples.mvp.controllers.start_screen;
 
+import examples.mvp.model.domainObjects.RecentItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
 public class RecentItemsController {
 
 
     // Variables
+    @FXML
+    private VBox recentItems_VBox;
+    private Stage parentStage;
 
 
     // Constructors
@@ -11,11 +22,32 @@ public class RecentItemsController {
 
     // Getters and Setters
 
+    public Stage getParentStage() {
+        return parentStage;
+    }
+
+    public void setParentStage(Stage parentStage) {
+        this.parentStage = parentStage;
+    }
+
 
     // Initialisation methods
 
 
     // Other methods
+    public void addRecentItems(ArrayList<RecentItem> recentItemsList) {
+        for (RecentItem recentItem : recentItemsList) {
+            Button itemButton = createButton(recentItem);
+            recentItems_VBox.getChildren().add(itemButton);
+        }
+    }
 
+    private Button createButton(RecentItem recentItem) {
+        Button temp = new Button(recentItem.getButtonText());
+        temp.setOnAction(event -> {
+            System.out.println(recentItem.getItemPath());
+        });
+        return temp;
+    }
 
 }
