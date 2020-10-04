614f5fe1298998f3f4e2e8e4c53db804e4a09ca1
Jack Grimes <flangojack@hotmail.com>
2020-10-04 12:39:56 +0100

    Placeholder pop-up for preferences configuration.

---
+++ src/main/java/examples/mvp/utils/gui/PreferencesPopUp.java
@@ -0,0 +1,21 @@
+package examples.mvp.utils.gui;
+
+public class PreferencesPopUp {
+
+
+    // Variables
+
+
+    // Constructors
+
+
+    // Getters and Setters
+
+
+    // Initialisation methods
+
+
+    // Other methods
+
+
+}
